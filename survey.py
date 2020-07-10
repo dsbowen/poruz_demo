5a66b278aa7f5ff7f5506bad8e433aede24049fc
dsbowen <dsbowen@wharton.upenn.edu>
2020-07-10 12:29:43 -0400

    updated for hemlock 0.0.16

--- survey.py
+++ survey.py
@@ -1,34 +1,5 @@
-"""Hemlock survey template"""
-
-from hemlock import *
-
-from random import shuffle
+from hemlock import Branch, Page, Label, route
 
 @route('/survey')
-def Start(origin=None):
-    b = Branch()
-    
-    p = Page(b)
-    Label(p, label='<p>This is a consent form.</p>')
-    Input(p, label='<p>Enter your MTurk ID.</p>', var='MTurkID', all_rows=True)
-
-    p = Page(b)
-    c = Check(
-        p,
-        label='<p>Which type of social media do you like most?</p>',
-        choices=['Facebook','Twitter','Instagram'],
-        var='SocialMedia'
-    )
-    shuffle(c.choices)
-    Validate.require(c)
-
-    p = Page(b)
-    i = Input(p, label='<p>Enter your name.</p>', var='Name', all_rows=True)
-    Validate.require(i)
-    i = Input(p, label='<p>Enter your age.</p>', var='Age', all_rows=True)
-    Validate.is_type(i, int)
-    Validate.min_val(i, 0)
-
-    p = Page(b, terminal=True)
-    Label(p, label='<p>Your responses have been recorded. Your payment code is xyz.</p>')
-    return b
+def start():
+    return Branch(Page(Label('<p>Hello World</p>'), terminal=True))
