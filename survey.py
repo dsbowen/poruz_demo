b474f07e28456614279e047df2e4f4850277ee81
dsbowen <dsbowen@wharton.upenn.edu>
2020-07-28 15:37:09 -0400

    with app.json

--- survey.py
+++ survey.py
@@ -2,4 +2,9 @@ from hemlock import Branch, Page, Label, route
 
 @route('/survey')
 def start():
-    return Branch(Page(Label('<p>Hello World</p>'), terminal=True))
+    return Branch(
+        Page(
+            Label('<p>Hello World</p>'), 
+            terminal=True
+        )
+    )
